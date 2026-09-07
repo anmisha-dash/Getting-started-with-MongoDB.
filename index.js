d6dd84fac12d216773065ea7aea5b53d32487e2c
Anmisha Dash <dashanmisha@gmail.com>
2026-09-07 17:39:36 +0530

    inserting multiple data

--- index.js
+++ index.js
@@ -19,22 +19,28 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model("User",userSchema);
 
-const user1 = new User({
-    name : "Adam",
-    email : "adam@yahoo.com",
-    age:48
+// const user1 = new User({
+//     name : "Adam",
+//     email : "adam@yahoo.com",
+//     age:48
+// });
+// const user2 = new User({
+//     name : "Eve",
+//     email : "evee@gmail.com",
+//     age: 18
+// })
+
+// user1.save();
+// user2.save().then((res)=>{console.log(res)})
+// .catch((err)=>{
+//     console.log(err);
+// })
+
+//many userss
+User.insertMany([
+    {name:"Anmisha",email:"anmisha@gmail.com",age:20},
+    {name:"Ashok",email:"ashok@gmail.com",age:25},
+    {name:"zooo",email:"zoo@gmail.com",age:18}
+]).then((res)=>{console.log(res);
 });
-const user2 = new User({
-    name : "Eve",
-    email : "evee@gmail.com",
-    age: 18
-})
-
-user1.save();
-user2.save().then((res)=>{console.log(res)})
-.catch((err)=>{
-    console.log(err);
-})
-
-
 // const Employee = mongoose.model("Employee",userSchema);
